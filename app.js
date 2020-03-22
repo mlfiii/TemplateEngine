6d1d191c4e531dc3008054c180ea28f0c7e61665
Vero Verto <gin@luthercorp.com>
2020-03-22 15:10:31 -0700

    Finished coding for basic app.

--- app.js
+++ app.js
@@ -1,109 +1,174 @@
+//Specify which modules and files to require
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
+//Where will files resolve to
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
 
+
+
+//Employee array
+let employees = []
 
 // Write code to use inquirer to gather information about the development team members,
-//Adapted from http://www.penandpaperprogrammer.com/blog/2018/12/16/repeating-questions-with-inquirerjs
-const collectEmployeeInfo = async (inputs = []) => {
-    const prompts = [
+//Used to ask questions about employees
+function collectEmployeeInfo() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "whichEmployeeType",
+            message: "Please select which employee type:",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
         {
             type: "input",
             name: "name",
-            message: "What is the name?"
+            message: "Employee name:"
         },
         {
             type: "input",
             name: "id",
-            message: "Manager's ID?"
-        },
-        {
-            type: "input",
-            name: "title",
-            message: "What is the title?"
+            message: "Employee ID:"
         },
         {
             type: "input",
             name: "email",
-            message: "What is the email?"
-        },
-        {
-            type: 'confirm',
-            name: 'again',
-            message: 'Enter another employee? ',
-            default: true
+            message: "Email:"
         }
-    ];
-
-    const { again, ...answers } = await inquirer.prompt(prompts);
-    const newInputs = [...inputs, answers];
-    return again ? collectEmployeeInfo(newInputs) : newInputs;
-};
-
-const main = async () => {
-    const inputs = await collectEmployeeInfo();
-    // console.log(inputs);
-};
-// and to create objects for each team member (using the correct classes as blueprints!)
+    ]).then((data) => {
+        const { whichEmployeeType } = data
 
 
+        switch (whichEmployeeType) {
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
-
-//Used to run the overall code.
-async function init() {
-
-    try {
-        // const numemps = await getHowManyEmployees();
-        // console.log(numemps)
-
-
-        const answers = await collectEmployeeInfo();
-
-
-
-        console.log(answers)
-
-
-        // const github = await getUserData(answers)
-
-        // const mdfile = await generateHTML(answers, github);
-
-        // await writeFileAsync("readme.md", mdfile);
-
-        // console.log("Successfully wrote to readme.md");
-    } catch (err) {
-        console.log(err);
-    }
+            case "Manager":
+                createManager(data)
+                break;
+            case "Engineer":
+                createEngineer(data)
+                break;
+            case "Intern":
+                createIntern(data)
+                break;
+            default:
+                break;
+        }
+    })
 }
 
-//Used too start he code.
-init();
+// and to create objects for each team member (using the correct classes as blueprints!)
+//Used to ask more questions about the manager employee and generate the html
+function createManager(employeeInfo) {
+    inquirer.prompt([{
+        type: "input",
+        message: "Office number?",
+        name: "officeNumber"
+    },
+    {
+        type: "confirm",
+        name: "moreEmployees",
+        message: "More employees?"
+    }
+    ]).then((data) => {
+
+        const { name, id, email } = employeeInfo
+        const { officeNumber } = data
+
+        //Create object
+        let newManager = new Manager(name, id, email, officeNumber)
+
+        //Push to the employee array
+        employees.push(newManager)
+
+        //If there are more employees to generate collect employee information otherwise go to generate html
+        if (data.moreEmployees) {
+            collectEmployeeInfo()
+        } else {
+            doneCreatingEmployees()
+        }
+    })
+}
+
+//Used to ask more questions about the engineer employee and generate the html
+function createEngineer(employeeInfo) {
+    inquirer.prompt([{
+        type: "input",
+        message: "Github login:",
+        name: "github"
+    },
+    {
+        type: "confirm",
+        name: "moreEmployees",
+        message: "More employees?"
+    }]).then((data) => {
+
+        const { name, id, email } = employeeInfo
+        const { github } = data
+
+        //Create object
+        let newEngineer = new Engineer(name, id, email, github)
+
+        //Push to the employee array
+        employees.push(newEngineer)
+
+        //If there are more employees to generate collect employee information otherwise go to generate html
+        if (data.moreEmployees) {
+            collectEmployeeInfo()
+        } else {
+            doneCreatingEmployees()
+        }
+    })
+}
+
+//Used to ask more questions about the intern employee and generate the html
+function createIntern(employeeInfo) {
+    inquirer.prompt([{
+        type: "input",
+        message: "School Name?",
+        name: "school"
+    },
+    {
+        type: "confirm",
+        name: "moreEmployees",
+        message: "More employees?"
+
+    }
+    ]).then((data) => {
+
+        const { name, id, email } = employeeInfo
+        const { school } = data
+
+        //Create object
+        let newIntern = new Intern(name, id, email, school)
+
+        //Push to the employee array
+        employees.push(newIntern)
+
+        //If there are more employees to generate collect employee information otherwise go to generate html
+        if (data.moreEmployees) {
+            collectEmployeeInfo()
+        } else {
+            doneCreatingEmployees()
+        }
+    })
+}
+
+function doneCreatingEmployees() {
+
+    fs.writeFile(outputPath, render(employees), (err, data) => {
+        if (err) throw err;
+
+        console.log("File written to folder")
+    })
+}
+
+//Used start to run the full application
+collectEmployeeInfo()
